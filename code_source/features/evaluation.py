2f32bcde5603ec79d1f986a4e7ffeefec599604c
svetlanacipiseva <svetlana.cipiseva@gmail.com>
2019-05-29 21:37:28 +0300

    10 commit

--- code_source/features/evaluation.py
+++ code_source/features/evaluation.py
@@ -6,11 +6,11 @@ interest_rate = 0.13
 lace = 0.05
 
 
-def calculate_revenue(cutoff, test_dataset, y_predict):
+def calculate_revenue(cutoff, test_dataset, y_predict, y_test, amounts):
     predict_paid_df = test_dataset[y_predict <= cutoff]  # predicted paid loans
     loan_count = predict_paid_df.shape[0]
-    amount_sum = predict_paid_df.Amount.sum()
-    default_rate = predict_paid_df.Default.mean()
+    amount_sum = amounts[y_predict <= cutoff].sum()
+    default_rate = y_test[y_predict <= cutoff].mean()
     revenue = amount_sum * (1 - default_rate) * (interest_rate / 12 + lace) - amount_sum * default_rate
 
     return pd.DataFrame({'Threshold': cutoff,
@@ -21,12 +21,12 @@ def calculate_revenue(cutoff, test_dataset, y_predict):
                         index=[0])
 
 
-def calculate_revenue_curve(test_dataset, y_predict):
-    fpr, tpr, cutoffs = roc_curve(test_dataset.Default, y_predict)
+def calculate_revenue_curve(test_dataset, y_predict, y_test, amounts):
+    fpr, tpr, cutoffs = roc_curve(y_test, y_predict)
 
     if len(cutoffs) > 200:
         sampled_indexes = np.linspace(0, len(cutoffs) - 1, num=200, dtype=np.int)
         cutoffs = cutoffs[sampled_indexes]
 
-    df_list = [calculate_revenue(cutoff, test_dataset, y_predict) for cutoff in cutoffs]
-    return pd.concat(df_list)
+    df_list = [calculate_revenue(cutoff, test_dataset, y_predict, y_test, amounts) for cutoff in cutoffs]
+    return pd.concat(df_list).reset_index()
